7cd1081114804eece05917dbdfaab63a85d4c1c1
Alex Bundy <abundy@opuslogica.com>
2020-03-24 15:14:12 -0700

    Fix imports in SurfacePolygonEditTool
    Also reformat code

--- SurfacePolygonEditTool.js
+++ SurfacePolygonEditTool.js
@@ -1,10 +1,7 @@
 define([
-    'OpusWorldWind/OpusWorldWind',
-    'WebWorldWind/WorldWind',
-    'WebWorldWind/geom/Position',
     'OpusWorldWind/AbstractSurfacePathEditTool'
-], function(WorldWind, Position, AbstractSurfacePathEditTool) {
-    var SurfacePolygonEditTool = function(wwd, polygon) {
+], function (AbstractSurfacePathEditTool) {
+    var SurfacePolygonEditTool = function (wwd, polygon) {
         AbstractSurfacePathEditTool.call(this, wwd, polygon);
 
         this._dragBeginInfo = null;
@@ -16,25 +13,26 @@ define([
 
     SurfacePolygonEditTool.prototype = Object.create(AbstractSurfacePathEditTool.prototype);
 
-    SurfacePolygonEditTool.prototype.getPositions = function() {
+    SurfacePolygonEditTool.prototype.getPositions = function () {
         return this.renderables[0].boundaries;
     };
 
-    SurfacePolygonEditTool.prototype.setPositions = function(positions) {
+    SurfacePolygonEditTool.prototype.setPositions = function (positions) {
         this.renderables[0].boundaries = positions;
     };
 
-    SurfacePolygonEditTool.prototype.getMinimumRequiredPositions = function() {
+    SurfacePolygonEditTool.prototype.getMinimumRequiredPositions = function () {
         return 3;
     };
 
-    SurfacePolygonEditTool.prototype.isLoop = function() {
+    SurfacePolygonEditTool.prototype.isLoop = function () {
         return true;
     };
 
-    SurfacePolygonEditTool.prototype._renderableDragBegan = function(renderable, recognizer) {
+    SurfacePolygonEditTool.prototype._renderableDragBegan = function (renderable, recognizer) {
         var pickedTerrain = this.wwd.pickTerrain(this.wwd.canvasCoordinates(recognizer.clientX, recognizer.clientY)).objects[0];
-        if(pickedTerrain) {
+        if (pickedTerrain)
+        {
             var positions = angular.copy(this.getPositions());
             this._dragStartInfo = {
                 pickedTerrainPosition: pickedTerrain.position,
@@ -43,36 +41,44 @@ define([
         }
     };
 
-    SurfacePolygonEditTool.prototype._renderableDrag = function(renderable, recognizer, ended) {
-        if(renderable !== this.renderables[0]) {
+    SurfacePolygonEditTool.prototype._renderableDrag = function (renderable, recognizer, ended) {
+        if (renderable !== this.renderables[0])
+        {
             return;
         }
         var pickedTerrain = this.wwd.pickTerrain(this.wwd.canvasCoordinates(recognizer.clientX, recognizer.clientY)).objects[0];
-        if(pickedTerrain) {
+        if (pickedTerrain)
+        {
             var deltaLat = pickedTerrain.position.latitude - this._dragStartInfo.pickedTerrainPosition.latitude;
             var deltaLon = pickedTerrain.position.longitude - this._dragStartInfo.pickedTerrainPosition.longitude;
             var positions = angular.copy(this.getPositions());
-            for(var i = 0; i !== positions.length; ++i) {
+            for (var i = 0; i !== positions.length; ++i)
+            {
                 var p_prev = this._dragStartInfo.positions[i];
                 var p = positions[i];
                 p.latitude = p_prev.latitude + deltaLat;
                 p.longitude = p_prev.longitude + deltaLon;
-                if(p.longitude > 180) {
+                if (p.longitude > 180)
+                {
                     p.longitude = -180 + (p.longitude - 180);
-                } else if(p.longitude < -180) {
+                } else if (p.longitude < -180)
+                {
                     p.longitude = 180 + (p.longitude + 180);
                 }
             }
-            var allowChange = function() {
-                for(var i = 0; i !== positions.length; ++i) {
+            var allowChange = function () {
+                for (var i = 0; i !== positions.length; ++i)
+                {
                     var pos = positions[i];
-                    if(pos.latitude > 90 || pos.latitude < -90 || pos.longitude < -180 || pos.longitude > 180) {
+                    if (pos.latitude > 90 || pos.latitude < -90 || pos.longitude < -180 || pos.longitude > 180)
+                    {
                         return false;
                     }
                 }
                 return true;
             };
-            if(allowChange()) {
+            if (allowChange())
+            {
                 this.setPositions(positions);
                 this.updateHandles();
                 this.emit('update', ended);
@@ -81,11 +87,11 @@ define([
         }
     };
 
-    SurfacePolygonEditTool.prototype._renderableDragChanged = function(renderable, recognizer) {
+    SurfacePolygonEditTool.prototype._renderableDragChanged = function (renderable, recognizer) {
         this._renderableDrag(renderable, recognizer, false);
     };
 
-    SurfacePolygonEditTool.prototype._renderableDragEnded = function(renderable, recognizer) {
+    SurfacePolygonEditTool.prototype._renderableDragEnded = function (renderable, recognizer) {
         this._renderableDrag(renderable, recognizer, true);
         this._dragStartInfo = null;
     };
